def3d1e192640c78746e84e1b062bfcbf35c2d06
Tommaso Soru <mommi84@gmail.com>
2017-12-03 15:25:41 +0100

    main class

---
+++ src/main/java/org/aksw/gpaba/GPABAMain.java
@@ -0,0 +1,14 @@
+package org.aksw.gpaba;
+
+/**
+ * @author Tommaso Soru {@literal tsoru@informatik.uni-leipzig.de}
+ *
+ */
+public class GPABAMain {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
