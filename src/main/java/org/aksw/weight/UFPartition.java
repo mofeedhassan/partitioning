bc96b93221cd4c1f5919f7d39882c77d5b6636cf
Andre Valdestilhas <firmao@gmail.com>
2018-02-10 19:15:10 +0100

    Implementation of the interface Partition

---
+++ src/main/java/org/aksw/weight/UFPartition.java
@@ -0,0 +1,39 @@
+package org.aksw.weight;
+
+import java.util.Set;
+
+public class UFPartition implements Partition {
+
+	Set<PTSPNode> data = null;
+	
+	public UFPartition(Set<PTSPNode> node) {
+		data = node;
+	}
+
+	public Set<PTSPNode> getData() {
+		return data;
+	}
+
+	public void setData(Set<PTSPNode> data) {
+		this.data = data;
+	}
+
+	@Override
+	public int getNumberOfNodes() {
+		return data.size();
+	}
+
+	@Override
+	public int getSumOfNodesWeights() {
+		int ret = 0;
+		for (PTSPNode node : data) {
+			ret += node.getWeight();
+		}
+		return ret;
+	}
+
+	@Override
+	public String toString() {
+		return data.toString();
+	}
+}
